8457c1fb6515ddf7b4e8011c78836c073249edbd
Camila Fernandez <56271504+camferna@users.noreply.github.com>
2024-05-19 19:41:44 +0200

    Update coxed_perccensorship.R

--- coxed_perccensorship.R
+++ coxed_perccensorship.R
@@ -12,6 +12,6 @@ for (pc in perc_cens){
   simdata <- sim.survdata(N = 1000, T=500, xvars = 12, censor = pc, num.data.frames=100)
   for (i in 1:100){
     savedata <- data.frame(simdata[[i]]$data)
-    write.csv(savedata,paste0("~/Dropbox/Mac/Documents/Workspace/Simulations ensemble/percentage of censorship/percensdata_", pc,"_", i,".csv"))
+    write.csv(savedata,paste0("percensdata_", pc,"_", i,".csv"))
   }
 }
