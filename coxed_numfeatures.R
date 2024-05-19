35729084a413c044ed9341e14c341318cae158c6
Camila Fernandez <56271504+camferna@users.noreply.github.com>
2024-05-19 19:42:36 +0200

    Update coxed_numfeatures.R

--- coxed_numfeatures.R
+++ coxed_numfeatures.R
@@ -1,5 +1,5 @@
 #This script is to generate the datasets for the experiment
-#where we vary the number of samples
+#where we vary the number of features
 
 
 library(coxed)
@@ -13,8 +13,7 @@ for (nf in num_feature){
   simdata <- sim.survdata(N=1000, T=500, xvars = nf, censor=.5, num.data.frames=100)
   for (i in 1:100){
     savedata <- data.frame(simdata[[i]]$data)
-    
-    write.csv(savedata,paste0("C:/Users/camferna/Documents/Workspace/Simulations Ensemble/number of features/numfeatdata_", nf,"_", i,".csv"))
-    #write.csv(savedata,paste0("~/Dropbox/Mac/Documents/Workspace/Simulations ensemble/number of features/numfeatdata_", nf,"_", i,".csv"))
+    write.csv(savedata,paste0("numfeatdata_", nf,"_", i,".csv"))
+
   }
 }
