541c8d4f2eb52bd01191ce0dd781ec4eb3f09919
Camila Fernandez <56271504+camferna@users.noreply.github.com>
2024-05-19 19:42:01 +0200

    Update coxed_numsamples.R

--- coxed_numsamples.R
+++ coxed_numsamples.R
@@ -11,6 +11,6 @@ for (ns in num_sample){
   simdata <- sim.survdata(N=ns, T=500, xvars = 12, censor=.5, num.data.frames=100)
   for (i in 1:100){
     savedata <- data.frame(simdata[[i]]$data)
-    write.csv(savedata,paste0("~/Dropbox/Mac/Documents/Workspace/Simulations ensemble/number of samples/numsampledata_", ns,"_", i,".csv"))
+    write.csv(savedata,paste0("numsampledata_", ns,"_", i,".csv"))
   }
 }
