7d90043296427c9e5130cedf64a18efaab169b5e
Camila Fernandez <56271504+camferna@users.noreply.github.com>
2024-05-19 19:39:58 +0200

    Update simsurv_numsamples.R

--- simsurv_numsamples.R
+++ simsurv_numsamples.R
@@ -1,16 +1,6 @@
 library('simsurv')
-#library('survival')
-#library('flexsurv')
-
-#check the grid because it is better up to 1000 I think.
-#num_sample <- seq(100,10000, by = 400)
-#length(num_sample)
 
 num_sample <- seq(50,2000, by = 130)
-length(num_sample)
-
-#num_sample <- seq(20,500, by = 34)
-#length(num_sample)
 
 
 for (N in num_sample){
@@ -26,8 +16,6 @@ for (N in num_sample){
                                  X11=-0.5,X12=-0.5),
                        x= cov, maxt=5)
     simdata<-merge(cov,simdata)
-    #Create a new path in windows 
-    write.csv(simdata,paste0("C:/Users/camferna/Documents/Workspace/Simulations_Ensemble/simsurv_numsamples/numsampledata_", N,"_", i,".csv"))
-    #write.csv(simdata,paste0("~/Dropbox/Mac/Documents/Workspace/Simulations ensemble/number of samples/numsampledata_", N,"_", i,".csv"))
+    write.csv(simdata,paste0("numsampledata_", N,"_", i,".csv"))
   }
 } 
